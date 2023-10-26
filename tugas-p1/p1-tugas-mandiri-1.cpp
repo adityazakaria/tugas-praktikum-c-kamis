ed175c32a2b0c4585894fe6a98d754e89317b928
adityazakaria <139231475+adityazakaria@users.noreply.github.com>
2023-10-26 01:26:54 -0700

    Add files via upload

---
+++ tugas-p1/p1-tugas-mandiri-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int nim = 2341002;
+	char* nama = "Aditya Zakaria ";
+	char* jurusan = "Informatika";
+	
+	printf("nim : %i \n", nim);
+	printf("nama : %s \n", nama);
+	printf("jurusan : %s", jurusan);
+	return 0;
+}
